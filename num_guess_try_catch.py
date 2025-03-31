0d31583abe5a1450913b5e84c9249c917f68cb4a
Enoch Oyeniyi <enoch.oyeniyi@stu.ocsb.ca>
2025-03-31 16:01:25 +0000

    num_guess_try_catch

---
+++ num_guess_try_catch.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Created by: Enoch O
+# Created on: March 28, 2025
+
+
+def main():
+    # This function uses a try statement.
+
+    # Get users input
+    integer_as_string = input("Enter an integer:")
+    print("")
+
+    # process users input
+    try:
+        integer_as_number = int("integer_as_string")
+        print("you entered an integer correctly")
+    except Exception:
+        print("That was not an integer")
+    finally:
+        print("Thanks for playing")
+
+
+if __name__ == "__main__":
+    main()
